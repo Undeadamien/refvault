5691b9e5a7ff7dea72dac79a4fd8bffd28846aa7
Undeadamien <damien.trala@outlook.com>
2026-05-07 01:44:09 +0200

    add test for invalid url

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,14 @@ async def test_create_image(client):
     data = response.json()
     assert data["name"] == "Test Image"
     assert data["url"] == "https://example.com/test.jpg"
+
+
+@pytest.mark.asyncio
+async def test_create_image_wrong_url(client):
+    payload = {
+        "url": "hellllo",
+        "name": "Test Image",
+        "tags": ["test"],
+    }
+    response = await client.post("/images/", json=payload)
+    assert response.status_code == 422
