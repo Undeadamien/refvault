57b9f3764f195edb7403962b55d183973750d44e
Undeadamien <damien.trala@outlook.com>
2026-05-09 13:20:54 +0200

    remove Base.metadata.create_all

--- src/main.py
+++ src/main.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
 
 import os
-from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
-from src.database import Base, engine
 from src.routes import images, tags
 
 SERVER_ADDR = os.getenv("SERVER_ADDR", "0.0.0.0")
 SERVER_PORT = int(os.getenv("SERVER_PORT", "8000"))
 
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-
-app = FastAPI(title="RefVault", lifespan=lifespan)
+app = FastAPI(title="RefVault")
 app.include_router(images.router)
 app.include_router(tags.router)
 
