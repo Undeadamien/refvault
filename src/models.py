bd40939b10b6ebc51324c652837e1997b98e68b3
Undeadamien <damien.trala@outlook.com>
2026-05-11 15:42:02 +0200

    update requirements.txt and refactor models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Table
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from src.database import Base
@@ -14,18 +16,18 @@ image_tags = Table(
 
 class Tag(Base):
     __tablename__ = "tags"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), nullable=False, unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
 
 class Image(Base):
     __tablename__ = "images"
-    id = Column(Integer, primary_key=True, index=True)
-    url = Column(String(255), nullable=False)
-    name = Column(String(255), nullable=False)
-    tags = relationship("Tag", secondary=image_tags, lazy="selectin")
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    url: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    tags: Mapped[list[Tag]] = relationship("Tag", secondary=image_tags, lazy="selectin")
 
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(
-        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
     )
