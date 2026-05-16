945ecc9c0434a7ab6fe8d3819537599a78eabf9f
Undeadamien <damien.trala@outlook.com>
2026-05-16 13:45:56 +0200

    fix tmux_session.sh by sourcing venv inside the session

--- tmux_session.sh
+++ tmux_session.sh
@@ -12,9 +12,6 @@ if tmux has-session -t "$NAME_SESSION" 2>/dev/null; then
 fi
 
 if [ ! -f .venv/bin/activate ]; then python -m venv .venv; fi
-source ".venv/bin/activate"
-if [ -f pyproject.toml ]; then pip install --no-cache-dir -e ".[dev]"; fi
-if [ -f requirements.txt ]; then pip install --no-cache-dir -r requirements.txt; fi
 
 tmux new-session -d -s "$NAME_SESSION"
 
@@ -26,6 +23,13 @@ tmux set-hook -t "$NAME_SESSION" after-new-window[1] "send-keys 'clear' Enter"
 tmux set-hook -t "$NAME_SESSION" after-split-window[0] "send-keys 'source ${PROJECT_DIR}/.venv/bin/activate' Enter"
 tmux set-hook -t "$NAME_SESSION" after-split-window[1] "send-keys 'clear' Enter"
 
-tmux send-keys -t "$NAME_SESSION:1.1" "source ${PROJECT_DIR}/.venv/bin/activate" Enter "clear" Enter
+tmux send-keys -t "$NAME_SESSION:1.1" "source ${PROJECT_DIR}/.venv/bin/activate" Enter
+if [ -f pyproject.toml ]; then
+    tmux send-keys -t "$NAME_SESSION:1.1" "pip install --no-cache-dir -e '.[dev]'" Enter
+fi
+if [ -f requirements.txt ]; then
+    tmux send-keys -t "$NAME_SESSION:1.1" "pip install --no-cache-dir -r requirements.txt" Enter
+fi
+tmux send-keys -t "$NAME_SESSION:1.1" "clear" Enter
 
 tmux attach -t "$NAME_SESSION:1.1"
