61c416b8b78c8e4236507d07c00d18f426306127
Undeadamien <damien.trala@outlook.com>
2026-05-18 16:28:56 +0200

    add tests for models

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.models import Image, Tag, User
+
+
+@pytest.mark.asyncio
+async def test_user():
+    user = User(username="alice", hashed_password=".")
+    assert str(user) == "alice"
+
+
+@pytest.mark.asyncio
+async def test_tag():
+    tag = Tag(name="nature")
+    assert str(tag) == "nature"
+
+
+@pytest.mark.asyncio
+async def test_image():
+    image = Image(url="placeholder", name="Beach", user_id=0)
+    assert str(image) == "Beach"
